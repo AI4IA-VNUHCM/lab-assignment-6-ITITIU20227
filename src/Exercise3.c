5a2182e406c12eb31a24dab102e8978c62255375
Truong Duc Duy Khang <ITITIU20227@student.hcmiu.edu.vn>
2021-11-22 15:02:42 +0700

    Week 6

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,53 @@ Ex:
 
 void Ex3(char *str){
 	//Your codes here
+	int len = strlen(str);
+	char longstring[1000], shortstring[1000];
+	int i = 0, longcount = 0, shortcount = 0, longest = 0, shortest = 1000;
+	int longindex, shortindex;
+	for(i = 0; i < len; i++) {
+		if(str[i] != ' ') {
+			shortcount++;
+			longcount++;
+		}
+		else {
+			if(shortcount < shortest) {
+				shortest = shortcount;
+				shortindex = i - shortest;
+			}
+			if(longcount > longest) {
+				longest = longcount;
+				longindex = i - longest;
+			}
+			shortcount = 0;
+			longcount = 0;
+		}
+	}
+	if(shortcount < shortest) {
+				shortest = shortcount;
+				shortindex = i - shortest;
+	}
+	if(longcount > longest) {
+				longest = longcount;
+				longindex = i - longest;
+	}
 	
+	int j = 0;
+	for(int i = longindex; i < (longindex + longest); i++) {
+		longstring[j] = str[i];
+		j++;
+	}
+
+	int k = 0;
+	for(int i = shortindex; i < (shortindex + shortest); i++) {
+		shortstring[k] = str[i];
+		k++;
+	}
+	shortstring[k] = '\0';
+	longstring[j] = '\0';
+
+	printf("Shortest word: %s\n", shortstring);
+	printf("Longest word: %s", longstring);
 }
 
 int main(int argc, char *argv[]) {
