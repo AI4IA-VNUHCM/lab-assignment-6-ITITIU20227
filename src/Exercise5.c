71dfba9615913c1bdbe26f0c0484f655b31ed9ab
Truong Duc Duy Khang <ITITIU20227@student.hcmiu.edu.vn>
2021-11-22 17:30:00 +0700

    Update Exercise 5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -23,44 +23,45 @@ void Ex5(char *str){
 	if(len % 2 != 0) {
 		printf("Invalid!");
 	}
-	
-	for(int i = 0; i < (len/2); i++) {
-		half1[i] = str[i];
-	}
-
-	int half2index = 0;
-	for(int j = len/2; j < len; j++) {
-		half2[half2index] = str[j];
-		half2index++;
-	}
-	half2[half2index] = '\0';
-
-	for(int i = 0; i < len/2; i++) {
-		if(half2[i] == ']') {
-			half2[i] = '[';
-		}
-		else if(half2[i] == '}') {
-			half2[i] = '{';
-		}
-		else if(half2[i] == ')') {
-			half2[i] = '(';
-		}
-	}
-
-	int j = 0;
-	for(int i = (len/2) - 1; i >= 0; i--) {
-		check[j] = half2[i];
-		j++;
-	}
-	check[j] = '\0';
-	
-	if(strcmp(check, half1) == 0) {
-		printf("Valid!");
-	}
 	else {
-		printf("Invalid!");
+		for(int i = 0; i < (len/2); i++) {
+			half1[i] = str[i];
+		}
+
+		int half2index = 0;
+		for(int j = len/2; j < len; j++) {
+			half2[half2index] = str[j];
+			half2index++;
+		}
+		half2[half2index] = '\0';
+
+		for(int i = 0; i < len/2; i++) {
+			if(half2[i] == ']') {
+				half2[i] = '[';
+			}
+			else if(half2[i] == '}') {
+				half2[i] = '{';
+			}
+			else if(half2[i] == ')') {
+				half2[i] = '(';
+			}
+		}
+
+		int j = 0;
+		for(int i = (len/2) - 1; i >= 0; i--) {
+			check[j] = half2[i];
+			j++;
+		}
+		check[j] = '\0';
+	
+		if(strcmp(check, half1) == 0) {
+			printf("Valid!");
+		}
+		else {
+			printf("Invalid!");
+		}
 	}
-}		
+}
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
